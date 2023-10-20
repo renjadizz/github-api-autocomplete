7396592114e215ef0ba6e965d4c6a5dd064198e7
Murat <renja.dizz@gmail.com>
2023-10-21 02:26:42 +0600

    format css

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 window.onload = (event) => {
 
-    const input = document.getElementById("searchField");
-    const searchDiv = document.getElementsByClassName("values");
+    const input = document.getElementsByClassName("search__input")[0];
+    const searchList = document.getElementsByClassName("search__list")[0];
 
     async function updateValue(e) {
-        clearSearchList(searchDiv[0]);
+        clearSearchList(searchList);
         if (e.target.value.trim().length) {
             const searchedValue = e.target.value;
             const githubResult = await fetch(`https://api.github.com/search/repositories?q=${searchedValue}&per_page=5`);
             const jsonData = await githubResult.json();
-            populateSearchList(jsonData.items, searchDiv[0]);
+            populateSearchList(jsonData.items, searchList);
         }
     }
 
     function populateSearchList(data, searchList) {
         let ulNode = document.createElement("ul");
+        ulNode.setAttribute("class", "search__list__ul");
         for (let item of data) {
             let liNode = document.createElement("li");
+            liNode.setAttribute("class", "search__list__ul__li");
             liNode.setAttribute("id", item.id);
             liNode.setAttribute("name", item.name);
             liNode.setAttribute("owner", item.owner.login);
             liNode.setAttribute("stars", item.stargazers_count);
             let aNode = document.createElement("a");
+            aNode.setAttribute("class", "search__list__ul__li__a");
             aNode.setAttribute("href", "#");
             aNode.textContent = item.name;
             liNode.append(aNode);
